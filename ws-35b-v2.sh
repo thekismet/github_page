e7fa956500aca3d7eb2ca935755d1c7fac38b942
thekismet <38202378+thekismet@users.noreply.github.com>
2021-03-01 17:47:47 +0900

    Update ws-35b-v2.sh

--- ws-35b-v2.sh
+++ ws-35b-v2.sh
@@ -16,29 +16,29 @@ sudo cp waveshare35b-v2-overlay.dtb /boot/overlays/waveshare35b-v2-overlay.dtbo
 
 sudo cp -rf usr/share/X11/xorg.conf.d/99-fbturbo.conf /usr/share/X11/xorg.conf.d/99-fbturbo.conf
 
-#sudo cp -rf etc/X11/xorg.conf.d/99-calibration.conf-35b /etc/X11/xorg.conf.d/99-calibration.conf
+sudo cp -rf etc/X11/xorg.conf.d/99-calibration.conf-35b /etc/X11/xorg.conf.d/99-calibration.conf
 
-#sudo cp -rf etc/X11/xorg.conf.d/99-calibration.conf-35b /usr/share/X11/xorg.conf.d/99-calibration.conf
+sudo cp -rf etc/X11/xorg.conf.d/99-calibration.conf-35b /usr/share/X11/xorg.conf.d/99-calibration.conf
 
-#sudo cat <<'EOF' > /etc/X11/xorg.conf.d/99-calibration.conf
-#Section "InputClass"
-#		Identifier      "calibration"
-#		MatchProduct    "ADS7846 Touchscreen"
-#		Option  "Calibration"   "3932 300 294 3801"
-#		Option  "SwapAxes"      "1"
-#		Driver  "evdev"
-#EndSection
-#EOF
+sudo cat <<'EOF' > /etc/X11/xorg.conf.d/99-calibration.conf
+Section "InputClass"
+		Identifier      "calibration"
+		MatchProduct    "ADS7846 Touchscreen"
+		Option  "Calibration"   "3932 300 294 3801"
+		Option  "SwapAxes"      "1"
+		Driver  "evdev"
+EndSection
+EOF
 
-#sudo cat <<'EOF' > /usr/share/X11/xorg.conf.d/99-calibration.conf
-#Section "InputClass"
-#		Identifier      "calibration"
-#		MatchProduct    "ADS7846 Touchscreen"
-#		Option  "Calibration"   "3932 300 294 3801"
-#		Option  "SwapAxes"      "1"
-#		Driver  "evdev"
-#EndSection
-#EOF
+sudo cat <<'EOF' > /usr/share/X11/xorg.conf.d/99-calibration.conf
+Section "InputClass"
+		Identifier      "calibration"
+		MatchProduct    "ADS7846 Touchscreen"
+		Option  "Calibration"   "3932 300 294 3801"
+		Option  "SwapAxes"      "1"
+		Driver  "evdev"
+EndSection
+EOF
 
 grep -q "dtparam=spi=on" /boot/userconfig.txt || \
   echo "dtparam=spi=on" >> /boot/userconfig.txt
